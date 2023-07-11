98f4d9ea4df01933f5effbed292277d9afff64b2
yusuf <bwitlawalyusuf@gmail.com>
2023-07-11 01:59:31 +0100

    email template footer links updated

--- application/views/email/footer.php
+++ application/views/email/footer.php
@@ -14,18 +14,18 @@
 			</div>
 			<div style="width:100%;text-align:center;padding:10px 0 20px 0;">
 				<ul style="min-width:100px;display:inline-block;margin:auto;list-style:none;margin:0;padding:0;">
-					<li style="position:relative;width:200px;border:1px solid #333;border-radius:225px;float:left;line-height:35px;margin-right:20px;font-family:calibri"><a style="display:block;text-decoration:none;color:#333" href=""><img style="position:absolute;top:10px;left:20px;float:left;width:20px;" src="https://dev-rent.smallsmall.com/assets/img/mobile-icons/apple.svg" /> <span>on Appstore</span></a></li>
-					<li style="position:relative;width:200px;border:1px solid #333;border-radius:225px;float:left;line-height:35px;font-family:calibri"><a style="display:block;text-decoration:none;color:#333" href="https://play.google.com/store/apps/details?id=com.JustifiedTech.smallsmall"><img style="position:absolute;top:10px;left:20px;float:left;width:15px;" src="https://dev-rent.smallsmall.com/assets/img/mobile-icons/google-play.svg" />on Google play</a></li>
+					<li style="position:relative;width:200px;border:1px solid #333;border-radius:225px;float:left;line-height:35px;margin-right:20px;font-family:calibri"><a style="display:block;text-decoration:none;color:#333" href=""><img style="position:absolute;top:10px;left:20px;float:left;width:20px;" src="https://smallsmall.com/assets/img/mobile-icons/apple.svg" /> <span>on Appstore</span></a></li>
+					<li style="position:relative;width:200px;border:1px solid #333;border-radius:225px;float:left;line-height:35px;font-family:calibri"><a style="display:block;text-decoration:none;color:#333" href="https://play.google.com/store/apps/details?id=com.JustifiedTech.smallsmall"><img style="position:absolute;top:10px;left:20px;float:left;width:15px;" src="https://smallsmall.com/assets/img/mobile-icons/google-play.svg" />on Google play</a></li>
 				</ul>
 			</div>
 			<div style="width:100%;text-align:center;line-height:30px;font-family:calibri;color:#03273B">Follow us</div>
 			<div style="width:100%;text-align:center;padding:10px 0 20px 0;">
 				<ul style="min-width:100px;display:inline-block;margin:auto;list-style:none;margin:0;padding:0;">
-					<li style="width:20px;height:20px;float:left;margin-right:30px;"><a style="display:block" href=""><img src="https://dev-rent.smallsmall.com/assets/img/mobile-icons/twitter.svg" /></a></li>
-					<li style="width:20px;height:20px;float:left;margin-right:30px;"><a style="display:block" href=""><img src="https://dev-rent.smallsmall.com/assets/img/mobile-icons/instagram.svg" /></a></li>
-					<li style="width:20px;height:20px;float:left;margin-right:30px;"><a style="display:block" href=""><img src="https://dev-rent.smallsmall.com/assets/img/mobile-icons/linkedin.svg" /></a></li>
-					<li style="width:20px;height:20px;float:left;margin-right:30px;"><a style="display:block" href=""><img src="https://dev-rent.smallsmall.com/assets/img/mobile-icons/facebook.svg" /></a></li>
-					<li style="width:20px;height:20px;float:left;"><a style="display:block" href=""><img src="https://dev-rent.smallsmall.com/assets/img/mobile-icons/youtube.svg" /></a></li>
+					<li style="width:20px;height:20px;float:left;margin-right:30px;"><a style="display:block" href=""><img src="https://smallsmall.com/assets/img/mobile-icons/twitter.svg" /></a></li>
+					<li style="width:20px;height:20px;float:left;margin-right:30px;"><a style="display:block" href=""><img src="https://smallsmall.com/assets/img/mobile-icons/instagram.svg" /></a></li>
+					<li style="width:20px;height:20px;float:left;margin-right:30px;"><a style="display:block" href=""><img src="https://smallsmall.com/assets/img/mobile-icons/linkedin.svg" /></a></li>
+					<li style="width:20px;height:20px;float:left;margin-right:30px;"><a style="display:block" href=""><img src="https://smallsmall.com/assets/img/mobile-icons/facebook.svg" /></a></li>
+					<li style="width:20px;height:20px;float:left;"><a style="display:block" href=""><img src="https://smallsmall.com/assets/img/mobile-icons/youtube.svg" /></a></li>
 				</ul>
 			</div>
 		</div>
